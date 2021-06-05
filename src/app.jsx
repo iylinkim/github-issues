89f37345485691db3a920c8babae043a28a486fb
iylinkim <kiylin94@naver.com>
2021-06-05 14:13:58 +0900

    Add router

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,23 @@
-import './app.css';
+import "./app.css";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/detail">
+            <Detail />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
